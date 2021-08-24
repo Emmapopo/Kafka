003147ec62a3228fe0536944b84daedabc2fbccf
Emmapopo <emmanueloyedeji20@yahoo.com>
2021-08-24 18:15:37 +0100

    updated kafka server with addconsumer endpoint

--- kafkaserver.py
+++ kafkaserver.py
@@ -6,16 +6,22 @@
 # ------------------------------------------------
 
 from flask import Flask, request
+
 import functions
 import variables
 from controller.producers import Producers
+from controller.consumers import Consumers
 from log.logging_func import logger
 
 producers = variables.producers
+consumers = variables.consumers
+assigner = variables.assigner
 
 app = Flask(__name__)
 
+
 producer_controller = Producers(producers)
+consumer_controller = Consumers(consumers, assigner)
 
 
 @app.route('/kafka/producer/add', methods=['POST'])
@@ -48,6 +54,45 @@ def add_producer():
     except:
         logger.error("Exception occured", exc_info=True)
         return{"status": f"{user_id} invalid field"}
+
+
+@app.route('/kafka/consumer/add', methods=['POST'])
+def add_consumer():
+    """ Endpoint to add a consumer to the kafka server"""
+    try:
+        json_data = request.get_json()
+        consumer_id = json_data["consumer_id"]
+        topic = json_data["topic"]
+        logger.info(
+            f'{consumer_id} tried to register as a consumer to topic: {topic}')
+
+    except Exception as e:
+        logger.error("InputFailed occured", exc_info=True)
+        return{"status": e}
+
+    try:
+        if not functions.is_valid_uuid(consumer_id):
+            logger.error(
+                f'{consumer_id} tried to register as a consumer with invalid UUID')
+            return {"status": "Invalid uuid"}, 400
+
+        if topic not in ["topic1", "topic2", "topic3"]:
+            logger.error(
+                f'{consumer_id} tried to register to invalid topic: {topic}')
+            return {"status": f"{topic} not valid"}, 400
+
+        status = consumer_controller.add(consumer_id, topic)
+        if status == "consumer already added":
+            logger.error(f'consumer: {consumer_id} registered before')
+            return {"status": status}, 409
+
+        else:
+            logger.info(f'{consumer_id} registered as consumer successfully')
+            return {"status": status}, 201
+
+    except:
+        logger.error("Exception occured", exc_info=True)
+        return{"status": "invalid field"}
 
 
 if __name__ == '__main__':
