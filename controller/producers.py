4e0e4a9da1516c78a2a074bc01ebd8cff5e7964c
Emmapopo <emmanueloyedeji20@yahoo.com>
2021-08-24 18:56:44 +0100

    change user_id to producer_id in producers controller

--- controller/producers.py
+++ controller/producers.py
@@ -7,11 +7,10 @@ class Producers(ProducerAbstract):
     def __init__(self, producers):
         self.producers = producers
 
-    def add(self, user_id):
+    def add(self, producer_id):
         """ function to add new producer"""
-        if user_id in self.producers:
-            return("user already added")
+        if producer_id in self.producers:
+            return("producer already added")
         else:
-            self.user_id = user_id
-            self.producers.append(self.user_id)
-            return(f"{self.user_id} added")
+            self.producers.append(producer_id)
+            return(f"{producer_id} added")
